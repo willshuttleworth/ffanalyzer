a5d2687b070009b7288f50e4107ed724952c6aec
willshuttleworth <93229335+willshuttleworth@users.noreply.github.com>
2021-10-28 09:27:08 -0400

    Add files via upload

--- fantasyStandings.js
+++ fantasyStandings.js
@@ -1,5 +1,20 @@
 //TODO:
-//ADD TO THE PRINTING METHODS? OR PRINTING TO SORTING METHODS
+//ADD FORM INPUT FOR LEAGUE ID
+//CHANGE TABLE RANKINGS WHEN RESPECTIVE BUTTON IS CLICKED
+//  CREATE METHOD FOR EACH SORTING TYPE
+//  OR
+//  CREATE ONE METHOD AND PASS IN SORTING TYPE
+
+//getting league id from variety of input links:
+//find indext of "leagueId="
+//get substring at indexOf + 9
+//add numbers to id string, stop at end of string OR character is not a number
+//try catch or something to see if id is valid
+//could be invalid at 2 steps:
+//  1. link does not include a league id
+//  2. given league id returns some sort of error
+
+//set cooldown on button
 
 class Team{
   static standings = []
@@ -46,23 +61,17 @@ class Team{
   this.percentDiff = winPercent - cumWinPercent
   }
 }
-
-//money league
-const apiURL1 = "https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/segments/0/leagues/622216888?view=mBoxscore"
-//oven league
-const apiURL2 = "https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/segments/0/leagues/1038617?view=mBoxscore"
-
-
-//array of Team objects
-let teams = []
-let currentWeek = 0
-
   //GETTING POINTS FOR FROM API
-  async function getMatchupData()
+  async function getMatchupData(apiURL1, apiURL2)
   {
     const response = await fetch(apiURL1)
     const matchupData = await response.json()
 
+    const response2 = await fetch(apiURL2);
+    const leagueData = await response2.json();
+    const leagueName = leagueData.settings.name;
+
+    document.getElementById('header').innerText = leagueName;
     //hash table with "espn id": "default id"
     let idKeys = []
 
@@ -82,11 +91,13 @@ let currentWeek = 0
       scores[idKeys[matchupData.schedule[i].home.teamId]][matchupData.schedule[i].matchupPeriodId - 1] = matchupData.schedule[i].home.totalPoints
     }
 
+    //resetting teams array
+    teams = new Array();
     //initializing array of Team objects
     for(let i = 0; i < scores.length; i++)
       teams[i] = new Team(matchupData.teams[i].location + " " + matchupData.teams[i].nickname, matchupData.teams[i].id, scores[i], matchupData.teams[i].record.overall.wins, matchupData.teams[i].record.overall.ties, matchupData.teams[i].record.overall.losses)
 
-
+    console.log(teams)
     for(let i = 0; i < teams.length; i++){
       teams[i].calculateCumRecord(teams, i)
       teams[i].calculatePercentDiff()
@@ -94,6 +105,30 @@ let currentWeek = 0
 
     //ranking teams based on cumulative record
     teams = createCumRankings(teams)
+    var table = document.getElementById('table')
+
+    //adding rankings to html table
+    for(let i = 0; i < teams.length; i++){
+      var row = table.insertRow(i + 1)
+      var record = teams[i].wins + "-" + teams[i].draws + "-" + teams[i].losses;
+      var cumRecord = teams[i].cumWins + "-" + teams[i].cumDraws + "-" + teams[i].cumLosses;
+
+      row.insertCell(0).innerText = i + 1;
+      row.insertCell(1).innerText = teams[i].fullName;
+      row.insertCell(2).innerText = cumRecord;
+      row.insertCell(3).innerText = record;
+      row.insertCell(4).innerText = teams[i].percentDiff.toFixed(3)
+    }
+
+    //removing excess rows
+    let tableLength = table.rows.length;
+    console.log(tableLength + " " + teams.length)
+    let excessRows = tableLength - (teams.length + 1);
+
+    if(excessRows > 0){
+      for(let i = 0; i < excessRows; i++)
+        table.deleteRow(teams.length + 1);
+    }
     console.log("cumulative standings")
 
     for(let i = 0; i < teams.length; i++)
@@ -147,4 +182,34 @@ let currentWeek = 0
     return teams
   }
 
-getMatchupData()
+  async function isValidURL(URL){
+    //test if url returns valid api call
+  }
+
+  function sortByName(){
+    console.log("sorted by name")
+  }
+  function sortByCumRecord(){
+    console.log("sorted by cumulative record")
+  }
+  function sortByRecord(){
+    console.log("sorted by record")
+  }
+  function sortByFraudScore(){
+    console.log("sorted by fraud score")
+  }
+
+  function getLeagueId(){
+    let leagueId = document.getElementById('leagueInput').value;
+    if(leagueId != null){
+      //url for scores
+      let apiURL1 = "https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/segments/0/leagues/" + leagueId + "?view=mBoxscore"
+      //url for league name
+      let apiURL2 = "https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/segments/0/leagues/" + leagueId + "?view=player_wl"
+      getMatchupData(apiURL1, apiURL2);
+    }
+  }
+
+//array of Team objects
+let teams = []
+let currentWeek = 0
